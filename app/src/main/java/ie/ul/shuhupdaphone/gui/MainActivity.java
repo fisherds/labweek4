65f13bbb9576e91d29fc64ffd5a0b81f0fd7b644
David Fisher <fisherds@rose-hulman.edu>
2019-02-14 10:25:48 +0000

    Added missing code

--- app/src/main/java/ie/ul/shuhupdaphone/gui/MainActivity.java
+++ app/src/main/java/ie/ul/shuhupdaphone/gui/MainActivity.java
@@ -1,4 +1,4 @@
-/* Student name: 
+/* Student name:
  * Student id:
  * Partner name:
  * Partner id:
@@ -17,47 +17,46 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+
 import java.util.ArrayList;
+
 import ie.ul.shuhupdaphone.R;
 
 public class MainActivity extends Activity {
-	
-	Button addModuleButton;
-	Button addLectureButton;
-	Button addLabButton;
-	Button addFullScheduleButton;
-	Button viewScheduleButton;
-	OnSharedPreferenceChangeListener prefListener;
 
-	
-	/*
-	 * For now modules are stored in this moduleList. ArrayLists are ideal for this: the size does not need to be set apriori, 
-	 * elements can be added (at the end or in the desired location) and removed as needed. For now this ArrayList will be 
-	 * accessed from the other activities. As the MainActivities existence cannot be guaranteed it should be a static field. 
-	 * Hence the keywords 'public static'.
-	 */
-	public static ArrayList<Module> moduleList = new ArrayList<Module>();
+  /*
+   * For now modules are stored in this moduleList. ArrayLists are ideal for this: the size does not need to be set apriori,
+   * elements can be added (at the end or in the desired location) and removed as needed. For now this ArrayList will be
+   * accessed from the other activities. As the MainActivities existence cannot be guaranteed it should be a static field.
+   * Hence the keywords 'public static'.
+   */
+  public static ArrayList<Module> moduleList = new ArrayList<Module>();
+  Button addModuleButton;
+  Button addLectureButton;
+  Button addLabButton;
+  Button addFullScheduleButton;
+  Button viewScheduleButton;
+  OnSharedPreferenceChangeListener prefListener;
 
-
-	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_relative);
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main_relative);
 
 
-        addModuleButton = (Button) findViewById(R.id.newModuleButton);
-        addLectureButton = (Button) findViewById(R.id.addLectureButton);
-        viewScheduleButton = (Button) findViewById(R.id.viewScheduleButton);
-        addFullScheduleButton = (Button) findViewById(R.id.addFullSchedule);
-        addLabButton = (Button) findViewById(R.id.addLabButton);
-        
-        addModuleButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent addModuleActivityIntent = new Intent(getBaseContext(), SetupModuleActivity.class);
-				startActivity(addModuleActivityIntent);
-			}
-        });
+    addModuleButton = (Button) findViewById(R.id.newModuleButton);
+    addLectureButton = (Button) findViewById(R.id.addLectureButton);
+    viewScheduleButton = (Button) findViewById(R.id.viewScheduleButton);
+    addFullScheduleButton = (Button) findViewById(R.id.addFullSchedule);
+    addLabButton = (Button) findViewById(R.id.addLabButton);
+
+    addModuleButton.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent addModuleActivityIntent = new Intent(getBaseContext(), SetupModuleActivity.class);
+        startActivity(addModuleActivityIntent);
+      }
+    });
         	
 /*
         addLectureButton.setOnClickListener(new OnClickListener() {
@@ -68,77 +67,69 @@ public class MainActivity extends Activity {
 			}
         });
 */
-        viewScheduleButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent viewScheduleActivityIntent = new Intent(getBaseContext(), ViewScheduleActivity.class);
-				startActivity(viewScheduleActivityIntent);
-			}
-        });
+    viewScheduleButton.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent viewScheduleActivityIntent = new Intent(getBaseContext(), ViewScheduleActivity.class);
+        startActivity(viewScheduleActivityIntent);
+      }
+    });
 
-        addFullScheduleButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent viewScheduleActivityIntent = new Intent(getBaseContext(), AddFullScheduleActivity.class);
-				startActivity(viewScheduleActivityIntent);
-			}
-        });
-        
-        SharedPreferences myPrefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+    addFullScheduleButton.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent viewScheduleActivityIntent = new Intent(getBaseContext(), AddFullScheduleActivity.class);
+        startActivity(viewScheduleActivityIntent);
+      }
+    });
 
-		prefListener = new OnSharedPreferenceChangeListener() {
-			public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
-				updateUIFromPreferences(prefs);
-			}
-		};
-		myPrefs.registerOnSharedPreferenceChangeListener(prefListener);
-        
-        
-        
-        
+    SharedPreferences myPrefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+
+    prefListener = new OnSharedPreferenceChangeListener() {
+      public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+        updateUIFromPreferences(prefs);
+      }
+    };
+    myPrefs.registerOnSharedPreferenceChangeListener(prefListener);
+    updateUIFromPreferences(myPrefs);
+  }
+
+
+  @Override
+  protected void onRestoreInstanceState(Bundle savedInstanceState) {
+  }
+
+
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+  }
+
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.activity_main, menu);
+    return true;
+  }
+
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.preferences: {
+        Intent prefIntent = new Intent(this, AppPreferences.class);
+        startActivity(prefIntent);
+        break;
+      }
     }
+    return super.onOptionsItemSelected(item);
+  }
 
- 
-	
-
-	 @Override
-		protected void onRestoreInstanceState(Bundle savedInstanceState) {
-		}
-
-
-
-
-		@Override
-		protected void onSaveInstanceState(Bundle outState) {
-		}
-
-
-
-
-	@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+  private void updateUIFromPreferences(SharedPreferences prefs) {
+    String name = prefs.getString(AppPreferences.KEY_USERNAME, "");
+    if (!name.contentEquals("")) {
+      viewScheduleButton.setText("View " + name + "'s " + "Schedule");
     }
+  }
 
 
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-			case R.id.preferences: {
-				break;
-			}
-		}
-		return super.onOptionsItemSelected(item);
-	}
-	
-	private void updateUIFromPreferences(SharedPreferences prefs) {
-		String name = prefs.getString(AppPreferences.KEY_USERNAME,"");
-		if (!name.contentEquals("")) {
-			viewScheduleButton.setText("View " + name+"'s " + "Schedule");
-		}
-	}
-
-    
 }
